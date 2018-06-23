e258d152edbfa378821e78ed2c40ed0f76680e2f
Kgadaws88 <privatemessage88@gmail.com>
2018-06-23 20:38:50 +0200

    ver. 0.1

---
+++ src/main/java/com/kamilgadawski/appWeather/AppWeatherApplication.java
@@ -0,0 +1,12 @@
+package com.kamilgadawski.appWeather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWeatherApplication.class, args);
+	}
+}
